dea80cb1b7da39af23c44f404a9e5f1edb56e411
U-DESKTOP-ARD4MPG\oorzu <or.zuk@mail.huji.ac.il>
2017-04-23 19:30:14 +0300

    Fixed mcmc sampling

--- MCMC_Permutations.R
+++ MCMC_Permutations.R
@@ -6,38 +6,29 @@ MCMC_Permutations<-function(Dat,W,TargetSampleSize,N)
   Counter = 1;
   Idx = 1;
   PermutationsTable = matrix(0,N,TargetSampleSize);
-  oldPerm = 1:N;
+  Perm = 1:N; # init with ID permutation 
 
   while(Idx<=TargetSampleSize)
   {
     #Here we implement a MHS algorithm with target stationary distribution \pi
     #Choose the two indices to be switched
-    #note that this way we are choosing a neighbor at random with probability of 1/(n*(n-1))
-    newPerm = oldPerm;
-    switchIdx = sample(1:N, 2, replace = TRUE)
+    #note that this way we are choosing a neighboring permutation at random with probability of 2/(n*(n-1))
+    switchIdx = sample(1:N, 2, replace = FALSE)
   
     #Should we accept the new permutation ?
     i = switchIdx[1];
     j = switchIdx[2];
-    ratio = W[i,oldPerm[j]]*W[j,oldPerm[i]]/W[i,oldPerm[i]]*W[j,oldPerm[j]];
-    p_old_to_new = min(1,ratio)
-    #Toss a coin with probability p_old_to_new:
-    u = runif(1,0,1)
-  
-    if(u<p_old_to_new)#we accept the transition:
+    ratio = W[i,Perm[j]]*W[j,Perm[i]]/(W[i,Perm[i]]*W[j,Perm[j]]);
+
+    if(runif(1,0,1)<min(1,ratio)) # accept the transition: Toss a coin with prob. min(1,ratio)
     {
-      newPerm[switchIdx[1]] = oldPerm[switchIdx[2]];
-      newPerm[switchIdx[2]] = oldPerm[switchIdx[1]];
-    
+      temp <- Perm[i]; Perm[i] <- Perm[j]; Perm[j] <- temp;# swap i and j
       if(Counter==BurningTime || (Counter%%Cycle==0 && Counter>BurningTime))
       {
-        PermutationsTable[,Idx]=newPerm;
+        PermutationsTable[,Idx]=Perm;
         Idx = Idx+1;
       }
-    
       Counter = Counter+1;
-      oldPerm = newPerm;
-      
     }
   }
   return(PermutationsTable)
